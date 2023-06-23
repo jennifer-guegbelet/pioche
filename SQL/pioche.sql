021ad525d12f65eb48ecd046187bd4ee71ccd377
jennifer-guegbelet <jennyferguegbelet@gmail.com>
2023-06-23 22:37:47 +0200

    push base de données

---
+++ SQL/pioche.sql
@@ -0,0 +1,324 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : localhost:8889
+-- Généré le : ven. 23 juin 2023 à 20:36
+-- Version du serveur : 5.7.39
+-- Version de PHP : 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `pioche`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `auteur`
+--
+
+CREATE TABLE `auteur` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `date_naissance` date NOT NULL,
+  `nationalite` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `biographie` longtext COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `emprunt`
+--
+
+CREATE TABLE `emprunt` (
+  `id` int(11) NOT NULL,
+  `utilisateur_id` int(11) NOT NULL,
+  `exemplaire_id` int(11) DEFAULT NULL,
+  `date_emprunt` date NOT NULL,
+  `date_retour` date NOT NULL,
+  `livre_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `exemplaires`
+--
+
+CREATE TABLE `exemplaires` (
+  `id` int(11) NOT NULL,
+  `livre_id` int(11) DEFAULT NULL,
+  `statut` tinyint(1) NOT NULL,
+  `emplacement` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `genres`
+--
+
+CREATE TABLE `genres` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `genres`
+--
+
+INSERT INTO `genres` (`id`, `nom`) VALUES
+(1, 'Fantasy'),
+(2, 'Fiction'),
+(3, 'Roman'),
+(4, 'Horreur'),
+(5, 'Science-Fiction'),
+(6, 'Aventures'),
+(7, 'Documentaire'),
+(8, 'Biographie'),
+(9, 'Autobiographie'),
+(10, 'Poésie'),
+(11, 'Bande-dessinée(BD)'),
+(12, 'Manga');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `livre`
+--
+
+CREATE TABLE `livre` (
+  `id` int(11) NOT NULL,
+  `titre` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `auteur` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `annee_publication` int(11) NOT NULL,
+  `resume` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `isbn` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `langue` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `editeur` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `disponibilite` tinyint(1) NOT NULL,
+  `genre_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `livre`
+--
+
+INSERT INTO `livre` (`id`, `titre`, `auteur`, `annee_publication`, `resume`, `isbn`, `langue`, `editeur`, `disponibilite`, `genre_id`) VALUES
+(1, 'Black woman win', 'Naomie', 2023, 'Black Woman Win\" est une célébration vibrante de la force et de la résilience des femmes noires. Plongez dans cet ouvrage inspirant qui met en lumière leurs victoires triomphantes', '978-1-234567-89-0', 'Français', 'Naomie', 1, 7);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `messenger_messages`
+--
+
+CREATE TABLE `messenger_messages` (
+  `id` bigint(20) NOT NULL,
+  `body` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `headers` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `queue_name` varchar(190) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` datetime NOT NULL COMMENT '(DC2Type:datetime_immutable)',
+  `available_at` datetime NOT NULL COMMENT '(DC2Type:datetime_immutable)',
+  `delivered_at` datetime DEFAULT NULL COMMENT '(DC2Type:datetime_immutable)'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `reservation`
+--
+
+CREATE TABLE `reservation` (
+  `id` int(11) NOT NULL,
+  `utilisateur_id` int(11) DEFAULT NULL,
+  `livre_id` int(11) DEFAULT NULL,
+  `date_reservation` datetime NOT NULL,
+  `actif` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `utilisateur`
+--
+
+CREATE TABLE `utilisateur` (
+  `id` int(11) NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `nom` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `prenom` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `adresse` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `date_adhesion` date NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `roles` longtext COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '(DC2Type:json)'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `utilisateur`
+--
+
+INSERT INTO `utilisateur` (`id`, `email`, `nom`, `prenom`, `adresse`, `date_adhesion`, `password`, `roles`) VALUES
+(1, 'test@mail.com', 'Testeur', 'test', '12 rue de la liberté', '2023-06-23', '$2y$13$AIAz/Xh92CUQDg84eEmCX.6rSkwttCjiIVG5mYqs7p7HR7hk8YGGa', '[]');
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `auteur`
+--
+ALTER TABLE `auteur`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `emprunt`
+--
+ALTER TABLE `emprunt`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `IDX_364071D7FB88E14F` (`utilisateur_id`),
+  ADD KEY `IDX_364071D75843AA21` (`exemplaire_id`),
+  ADD KEY `IDX_364071D737D925CB` (`livre_id`);
+
+--
+-- Index pour la table `exemplaires`
+--
+ALTER TABLE `exemplaires`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `IDX_551C55F37D925CB` (`livre_id`);
+
+--
+-- Index pour la table `genres`
+--
+ALTER TABLE `genres`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `livre`
+--
+ALTER TABLE `livre`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `IDX_AC634F994296D31F` (`genre_id`);
+
+--
+-- Index pour la table `messenger_messages`
+--
+ALTER TABLE `messenger_messages`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `IDX_75EA56E0FB7336F0` (`queue_name`),
+  ADD KEY `IDX_75EA56E0E3BD61CE` (`available_at`),
+  ADD KEY `IDX_75EA56E016BA31DB` (`delivered_at`);
+
+--
+-- Index pour la table `reservation`
+--
+ALTER TABLE `reservation`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `IDX_42C84955FB88E14F` (`utilisateur_id`),
+  ADD KEY `IDX_42C8495537D925CB` (`livre_id`);
+
+--
+-- Index pour la table `utilisateur`
+--
+ALTER TABLE `utilisateur`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `UNIQ_1D1C63B3E7927C74` (`email`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `auteur`
+--
+ALTER TABLE `auteur`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `emprunt`
+--
+ALTER TABLE `emprunt`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `exemplaires`
+--
+ALTER TABLE `exemplaires`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `genres`
+--
+ALTER TABLE `genres`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT pour la table `livre`
+--
+ALTER TABLE `livre`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT pour la table `messenger_messages`
+--
+ALTER TABLE `messenger_messages`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `reservation`
+--
+ALTER TABLE `reservation`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `utilisateur`
+--
+ALTER TABLE `utilisateur`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `emprunt`
+--
+ALTER TABLE `emprunt`
+  ADD CONSTRAINT `FK_364071D737D925CB` FOREIGN KEY (`livre_id`) REFERENCES `livre` (`id`),
+  ADD CONSTRAINT `FK_364071D75843AA21` FOREIGN KEY (`exemplaire_id`) REFERENCES `exemplaires` (`id`),
+  ADD CONSTRAINT `FK_364071D7FB88E14F` FOREIGN KEY (`utilisateur_id`) REFERENCES `utilisateur` (`id`);
+
+--
+-- Contraintes pour la table `exemplaires`
+--
+ALTER TABLE `exemplaires`
+  ADD CONSTRAINT `FK_551C55F37D925CB` FOREIGN KEY (`livre_id`) REFERENCES `livre` (`id`);
+
+--
+-- Contraintes pour la table `livre`
+--
+ALTER TABLE `livre`
+  ADD CONSTRAINT `FK_AC634F994296D31F` FOREIGN KEY (`genre_id`) REFERENCES `genres` (`id`);
+
+--
+-- Contraintes pour la table `reservation`
+--
+ALTER TABLE `reservation`
+  ADD CONSTRAINT `FK_42C8495537D925CB` FOREIGN KEY (`livre_id`) REFERENCES `livre` (`id`),
+  ADD CONSTRAINT `FK_42C84955FB88E14F` FOREIGN KEY (`utilisateur_id`) REFERENCES `utilisateur` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
